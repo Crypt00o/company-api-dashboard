45bb030807d4496c1132092fc3d4eca4e72a62fc
Crypt00o <octopus2672001@gmail.com>
2023-03-19 19:46:09 +0200

    create database main.db & add config.rs which config default db env var

---
+++ src/config.rs
@@ -0,0 +1,19 @@
+use std::env::{vars,set_var};
+use std::collections::HashMap;
+use dotenv::dotenv;
+
+
+pub fn config_env(){
+
+    dotenv().ok();
+    
+    let mut env_map : HashMap<String, String>=HashMap::<String,String>::new();
+    
+    for (key,value) in vars() {
+        env_map.insert(key, value);
+    }
+
+    if !env_map.contains_key(&"SQLITE_MAIN_DB".to_string()){
+        set_var("SQLITE_MAIN_DB", "sqlite:../databases/main.db");
+    }
+}
