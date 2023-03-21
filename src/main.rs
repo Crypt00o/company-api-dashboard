36dfc64e5278167b319e1a8d5fd721ea39b74f29
Crypt00o <octopus2672001@gmail.com>
2023-03-21 07:42:57 +0200

    format source code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 mod config;
+mod controllers;
 mod main_db_profile;
 mod routers;
-mod controllers;
-
 
 use config::config_env;
-use tide::{Result};
-
-
+use tide::Result;
 
 #[tokio::main]
-async fn main()->Result<()> {
+async fn main() -> Result<()> {
     config_env();
-    let mut app=tide::new();
-    app= routers::index::index_router(app).await;
+    let mut app = tide::new();
+    app = routers::index::index_router(app).await;
     app.listen("127.0.0.1:8080").await?;
     return Ok(());
 }
--- src/routers/index.rs
+++ src/routers/index.rs
@@ -1,8 +1,7 @@
-
-use tide::Server;
 use super::super::controllers::main_controller::hello_client;
+use tide::Server;
 
-pub async fn index_router(mut app: Server<()>)->Server<()>{
-app.at("/*").all(hello_client);
+pub async fn index_router(mut app: Server<()>) -> Server<()> {
+    app.at("/*").all(hello_client);
     return app;
 }
