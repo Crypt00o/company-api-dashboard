7ff3b4778966a08fbb06b5a5677637dbe35fd40c
Crypt00o <octopus2672001@gmail.com>
2023-03-21 06:34:43 +0200

    create tide app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod config;
 mod main_db_profile;
+use std::error::Error;
+
 use config::config_env;
+use tide;
 
 #[tokio::main]
-async fn main() {
+async fn main()->tide::Result<()> {
     config_env();
+    let mut app=tide::new();
+    app.listen("127.0.0.1:8080").await?;
+    return Ok(());
 }
